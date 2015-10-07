35b294d6e23ffda17f8a719107deaa896ac57dab
louislugas <louislugas@gmail.com>
2015-10-08 01:33:51 +0700

    keyboard animation sukses

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,14 +1,3 @@
 $(document).ready(function(){
         $('.slider').slider();
     });
-
-$(document).keydown(function(event) {
-    switch (event.which) {
-        case 37: // left arrow key
-           $('#div').animate({ 'left': '-=100' });
-           break;
-        case 39: // right arrow key
-           $('#div').animate({ 'left': '+=100' });
-           break;
-    }
-});
---
+++ assets/js/slider2.js
@@ -0,0 +1,13 @@
+$(document).on( "keydown", function(event) {
+   var x = event.keyCode;
+    if (x == 37) {  // left
+    	$("div").animate({
+        left: '-=100px',
+		}); 
+	} else if (x == 39) {  // right
+		$("div").animate({
+        left: '+=100px',
+		}); 
+	}
+});
+
