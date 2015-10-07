bccda322e118d6eb283853e2b35ac4fdc4c052d0
louislugas <louislugas@gmail.com>
2015-10-07 19:49:46 +0700

    animation test

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
         $('.slider').slider();
     });
+
+$(document).keydown(function(event) {
+    switch (event.which) {
+        case 37: // left arrow key
+           $('#div').animate({ 'left': '-=100' });
+           break;
+        case 39: // right arrow key
+           $('#div').animate({ 'left': '+=100' });
+           break;
+    }
+});
