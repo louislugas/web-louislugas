bd95fac99e30add69feeed2045d0892fb5b4ad74
Adli Fariz Bonaputra <abathz@gmail.com>
2015-10-08 21:26:44 +0700

    apdet terbaru dari abat ganteng

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
         $('.slider').slider();
-    });
+});
+
+$(document).on( "keydown", function(event) {
+   var x = event.keyCode;
+    if (x == 37) {  // left
+    	$("div").animate({
+        left: '-=100px',
+		}); 
+	} else if (x == 39) {  // right
+		$("div").animate({
+        left: '+=100px',
+		}); 
+	}
+});
