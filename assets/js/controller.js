d68e28b64554dfa37f99109fc06b35bb69c397c6
Adli Fariz Bonaputra <abathz@gmail.com>
2015-10-14 21:04:44 +0700

    angular apdet

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,1 +1,15 @@
 var appControllers = angular.module('appControllers', []);
+
+appControllers.controller('PhoneListCtrl', function ($scope) {
+  $scope.project = [
+    {'name': 'Nexus S',
+     'imgUrl': 'img/work.jpg'
+ 	},
+ 	{'name': 'Motorolla A',
+     'imgUrl': 'img/work.jpg'
+ 	},
+ 	{'name': 'Samsung B',
+     'imgUrl': 'img/work.jpg'
+ 	}
+  ];
+});
